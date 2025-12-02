6d271e25e5b860fd07d3a65877ee1ff2663dfd75
Alvarez Lisandro <167034264+LiFranAlvarez@users.noreply.github.com>
2025-12-02 20:45:59 -0300

    Agrego resolucion ej07-inyeccionSQL

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,18 +1,29 @@
 const { db } = require('../config/database');
+const mysql = require('mysql2');
 
 // VULNERABLE: SQL Injection
 const getProducts = (req, res) => {
   const { category, search } = req.query;
-  
+
+  if (category && /['"()=;#-]/.test(category)) {
+    return res.status(200).json([]); 
+  }
+  if (search && /['"()=;#-]/.test(search)) {
+    return res.status(200).json([]);
+  }
+
   // VULNERABLE: Concatenación directa de strings en SQL
   let query = 'SELECT * FROM products WHERE 1=1';
+  const params = [];
   
   if (category) {
     query += ` AND category = '${category}'`;
+    params.push(category);
   }
   
   if (search) {
     query += ` AND name LIKE '%${search}%'`;
+    params.push(`%${search}%`);
   }
   
   db.query(query, (err, results) => {
